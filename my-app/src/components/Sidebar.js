b97baa1a10ea29884ca566c4bc6be5ae15c355a7
Maximiliane Kaempffer <maximiliane.kaempffer@gmail.com>
2024-07-06 18:33:55 +0000

    Initial commit. Add Sidebar components with links

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <div> 
+            <a href="#">MyPhotos</a>
+            <a href="#">MyIllustrations</a>
+            <a href="#">MyPaintings</a>
+        </div>
+    );
+}
+
+export default Sidebar;
