b2d098d2e1e30506b41c5b00525ad5319238053c
Maximiliane Kaempffer <maximiliane.kaempffer@gmail.com>
2024-07-06 18:52:06 +0000

    Add styling to Sidebar component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import css from "./css/Sidebar.module.css";
 
 const Sidebar = () => {
     return (
-        <div> 
+        <div className = {css.sidebar}> 
             <a href="#">MyPhotos</a>
             <a href="#">MyIllustrations</a>
             <a href="#">MyPaintings</a>
